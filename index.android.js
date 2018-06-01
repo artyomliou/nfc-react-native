404a10b6534d2260047abda65164962401ef278b
Yves Liou <bros0215@gmail.com>
2018-06-01 17:58:25 +0800

    Connect api via bridge

--- index.android.js
+++ index.android.js
@@ -2,6 +2,8 @@ import React from 'react-native';
 
 const NfcReactNative = React.NativeModules.NfcReactNative;
 
-export const readTag = NfcReactNative.readTag;
-export const writeTag = NfcReactNative.writeTag;
-export const getTagId = NfcReactNative.getTagId;
+export const setKeys = NfcReactNative.setKeys;
+export const connect = NfcReactNative.connect;
+export const close = NfcReactNative.close;
+export const read = NfcReactNative.read;
+export const write = NfcReactNative.write;
