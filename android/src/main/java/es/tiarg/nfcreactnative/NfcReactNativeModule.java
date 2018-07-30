d02ea07035f888adf2a72c0c43ecf5e47b3a8b8c
Yves Liou <bros0215@gmail.com>
2018-07-30 13:24:20 +0800

    Fix undefiend variables & undefiend methods

--- android/src/main/java/es/tiarg/nfcreactnative/NfcReactNativeModule.java
+++ android/src/main/java/es/tiarg/nfcreactnative/NfcReactNativeModule.java
@@ -142,7 +142,7 @@ class NfcReactNativeModule extends ReactContextBaseJavaModule implements Activit
     }
 
     @ReactMethod
-    public void read(ReadableMap map, Promise promise) {
+    public void read(ReadableMap param, Promise promise) {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
@@ -180,7 +180,7 @@ class NfcReactNativeModule extends ReactContextBaseJavaModule implements Activit
     }
 
     @ReactMethod
-    public void writeByte(ReadableMap map, Promise promise) {
+    public void writeByte(ReadableMap param, Promise promise) {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
@@ -225,7 +225,7 @@ class NfcReactNativeModule extends ReactContextBaseJavaModule implements Activit
     }
 
     @ReactMethod
-    public void close(ReadableMap map, Promise promise) {
+    public void close(Promise promise) {
         try {
             if (tag == null) {
                 throw new IOException("沒有抓到標籤");
--- index.android.js
+++ index.android.js
@@ -5,3 +5,4 @@ const NfcReactNative = React.NativeModules.NfcReactNative;
 export const setKey = NfcReactNative.setKey;
 export const read = NfcReactNative.read;
 export const writeByte = NfcReactNative.writeByte;
+export const close = NfcReactNative.close;
