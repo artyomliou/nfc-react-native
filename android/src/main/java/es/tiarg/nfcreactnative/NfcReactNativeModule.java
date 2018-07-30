44f57a086fc668ee3d427f20aef14ecdaac5263c
Yves Liou <bros0215@gmail.com>
2018-07-30 10:54:52 +0800

    Replace ScheduledExecutorService with AsyncTask to decrease lantency

--- android/src/main/java/es/tiarg/nfcreactnative/NfcReactNativeModule.java
+++ android/src/main/java/es/tiarg/nfcreactnative/NfcReactNativeModule.java
@@ -18,6 +18,7 @@ import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.MifareClassic;
 import android.util.Log;
+import android.os.AsyncTask;
 
 import java.lang.Exception;
 import java.io.UnsupportedEncodingException;
@@ -26,12 +27,7 @@ import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.util.Collections;
-import java.util.Queue;
-import java.util.LinkedList;
 import java.util.ArrayList;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import static com.facebook.common.util.Hex.hexStringToByteArray;
 
@@ -50,9 +46,6 @@ class NfcReactNativeModule extends ReactContextBaseJavaModule implements Activit
     private volatile String authKey = "FFFFFFFFFFFF";
     private volatile String authType = "B";
     private volatile ArrayList<Boolean> authStatuses;
-    private volatile Queue<String> operations;
-    private volatile Queue<ReadableMap> parameters;
-    private volatile Queue<Promise> promises;
 
     public NfcReactNativeModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -60,15 +53,8 @@ class NfcReactNativeModule extends ReactContextBaseJavaModule implements Activit
 
         tag = null;
 
-        operations = new LinkedList<String>();
-        parameters = new LinkedList<ReadableMap>();
-        promises = new LinkedList<Promise>();
-
         reactContext.addActivityEventListener(this);
         reactContext.addLifecycleEventListener(this);
-
-        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor(); 
-        exec.scheduleAtFixedRate(new OperationComsumerThread(), 0, 700, TimeUnit.MILLISECONDS); 
     }
 
     @Override
@@ -112,7 +98,6 @@ class NfcReactNativeModule extends ReactContextBaseJavaModule implements Activit
         // reset variables for further operation
         int sectorCount = tag.getSectorCount();
         resetTagInfos(sectorCount);
-        clearQueue();
 
         // pass info about this tag to JS
         WritableMap map = Arguments.createMap();
@@ -138,188 +123,6 @@ class NfcReactNativeModule extends ReactContextBaseJavaModule implements Activit
         adapter.disableForegroundDispatch(activity);
     }
 
-    private class OperationComsumerThread implements Runnable {
-        public void run() {
-            if (tag == null) {
-                return;
-            } else if (operations.size() == 0) {
-                return;
-            }
-            if (tag.isConnected() == false) {
-                try {
-                    tag.connect();
-                } catch (IOException e) {
-                    Log.d("ReactNative", e.getMessage());
-                    clearQueue(e.getMessage());
-                    return;
-                }
-            }
-
-            while (operations.size() > 0) {
-
-                String op = operations.element();
-                ReadableMap param = parameters.element();
-                Promise promise = promises.element();
-
-                int sectorIndex = param.getInt("sector");
-                int blockIndex = param.getInt("block");
-
-                Log.d("ReactNative", op);
-
-                try {
-                    switch (op) {
-                        case "read":
-                            doRead(sectorIndex, blockIndex, promise);
-                            break;
-                        case "writeByte":
-                            int byteIndex = param.getInt("byte");
-                            String data = param.getString("data");
-                            doWrite(sectorIndex, blockIndex, byteIndex, data, promise);
-                            break;
-                        }
-
-                    operations.remove();
-                    parameters.remove();
-                    promises.remove();
-                }
-                catch (Exception e) {
-                    Log.d("ReactNative", e.getMessage());
-                    promise.reject(E_LAYOUT_ERROR, Log.getStackTraceString(e));
-                    promises.remove();
-                    clearQueue(e.getMessage());
-                    break;
-                    // queue is cleared, no further operation.
-                    // loop stop here
-                }
-            }
-            
-            if (tag.isConnected()) {
-                try {
-                    tag.close();
-                } catch (IOException e) {
-                    WritableMap error = Arguments.createMap(); 
-                    error.putString("error", e.toString()); 
- 
-                    reactContext 
-                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class) 
-                        .emit("onTagError", error);
-                }
-            }
-        }
-
-        private void auth(int sectorIndex) throws Exception {
-            if (authStatuses.get(sectorIndex) == true) {
-                return;
-            }
-            boolean passed = false;
-            byte[][] arrayKeys = new byte[5][6];
-            arrayKeys[0] = hexStringToByteArray(authKey);
-            arrayKeys[1] = arrayKeys[0];
-            arrayKeys[2] = MifareClassic.KEY_DEFAULT;
-            arrayKeys[3] = MifareClassic.KEY_MIFARE_APPLICATION_DIRECTORY;
-            arrayKeys[4] = MifareClassic.KEY_NFC_FORUM;
-
-            String[] arrayTypes = new String[5];
-            arrayTypes[0] = authType;
-            arrayTypes[1] = "A".equals(authType) ? "B" : "A";
-            arrayTypes[2] = "A";
-            arrayTypes[3] = "A";
-            arrayTypes[4] = "A";
-
-            for (int i = 0; i < arrayKeys.length; i++) {
-                try {
-                    if ("A".equals(arrayTypes[i])) {
-                        passed = tag.authenticateSectorWithKeyA(sectorIndex, arrayKeys[i]);
-                    } else {
-                        passed = tag.authenticateSectorWithKeyB(sectorIndex, arrayKeys[i]);
-                    }
-                } catch (TagLostException e) {
-                    throw e;
-                } catch (IOException e) {
-                    Log.d("ReactNative", e.getMessage());
-                }
-                
-                if (passed) {
-                    Log.d("ReactNative", "Sector " + String.valueOf(sectorIndex) + ", " + byteArrayToHexString(arrayKeys[i]));
-                    break;
-                }
-            }
-            authStatuses.set(sectorIndex, passed);
-    
-            if (passed == false) {
-                throw new IOException("Authentication failed: sector" + String.valueOf(sectorIndex) + ", type" + authType + ", key=" + authKey);
-            }
-        }
-
-        private void doRead(int sectorIndex, int blockIndex, Promise promise) throws TagLostException, Exception {
-            auth(sectorIndex);
-
-            blockIndex = getRealBlockIndex(sectorIndex, blockIndex);
-            Log.d("ReactNative", String.valueOf(sectorIndex) + "," + String.valueOf(blockIndex));
-                
-            byte[] blockBytes = tag.readBlock(blockIndex);
-            String blockString = byteArrayToHexString(blockBytes);
-
-            WritableMap map = Arguments.createMap();
-            map.putString("payload", blockString);
-            promise.resolve(map);
-        }
-
-        private void doWrite(int sectorIndex, int blockIndex, int byteIndex, String data, Promise promise) throws TagLostException, Exception {
-            auth(sectorIndex);
-
-            blockIndex = getRealBlockIndex(sectorIndex, blockIndex);
-            Log.d("ReactNative", String.valueOf(sectorIndex) + "," + String.valueOf(blockIndex));
-            
-            byte[] blockBytes = tag.readBlock(blockIndex);
-            if (byteIndex > blockBytes.length - 1) {
-                throw new Exception("Out of bound: invalid byte index to wrtie");
-            } else {
-                blockBytes = overwriteBytesWithString(blockBytes, byteIndex, data);
-                tag.writeBlock(blockIndex, blockBytes);
-            }
-
-            WritableMap map = Arguments.createMap();
-            map.putString("payload", byteArrayToHexString(blockBytes));
-            promise.resolve(map);
-        }
-
-        /**
-         * When you want to access block 7 in sector 2
-         * (which usually means the last block in sector)
-         * You'll probably get blockIndex as 7 or 3.
-         * 
-         * When you get blockIndex as 3,
-         * you have to calculate the real blockIndex which Android API needs.
-         * 
-         * This function is designed to fix this.
-         */
-        private int getRealBlockIndex(int sectorIndex, int blockIndex) throws Exception {
-            int firstBlockInSector = tag.sectorToBlock(sectorIndex);
-            int blockCounts = tag.getBlockCountInSector(sectorIndex);
-            int lastBlockInSector = firstBlockInSector + blockCounts - 1;
-
-            if (firstBlockInSector <= blockIndex && blockIndex <= lastBlockInSector) {
-                // blockCounts = 4, sector = 3, block = 9
-                return blockIndex;
-            } else if (0 <= blockIndex && blockIndex <= blockCounts - 1) {
-                // blockCounts = 4, sector = 3, block = 0
-                return firstBlockInSector + blockIndex - 1;
-            } else {
-                throw new Exception("Out of bound: sector " + String.valueOf(sectorIndex) + ", block " + String.valueOf(blockIndex));
-            }
-        }
-
-        private byte[] overwriteBytesWithString(byte[] blockBytes, int byteIndex, String data) throws UnsupportedEncodingException {
-            byte[] bytes = data.getBytes("UTF-8");
-            for(byte b:bytes) {
-                blockBytes[byteIndex] = b;
-                byteIndex++;
-            }
-            return blockBytes;
-        }
-    }
-
     /**
      * @return the name of this module. This will be the name used to {@code require()} this module
      * from javascript.
@@ -340,43 +143,199 @@ class NfcReactNativeModule extends ReactContextBaseJavaModule implements Activit
 
     @ReactMethod
     public void read(ReadableMap map, Promise promise) {
-        operations.add("read");
-        parameters.add(map);
-        promises.add(promise);
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    connect();
+                } catch (IOException e) {
+                    Log.d("ReactNative", e.getMessage());
+                    promise.reject(E_LAYOUT_ERROR, Log.getStackTraceString(e));
+                    return;
+                }
+                try {
+                    int sectorIndex = param.getInt("sector");
+                    int blockIndex = param.getInt("block");
+
+                    blockIndex = getRealBlockIndex(sectorIndex, blockIndex);
+                    Log.d("ReactNative", String.valueOf(sectorIndex) + "," + String.valueOf(blockIndex));
+                    auth(sectorIndex);
+
+                    byte[] blockBytes = tag.readBlock(blockIndex);
+                    String blockString = byteArrayToHexString(blockBytes);
+
+                    WritableMap map = Arguments.createMap();
+                    map.putString("payload", blockString);
+                    promise.resolve(map);
+
+                } catch (IOException e) {
+                    Log.d("ReactNative", e.getMessage());
+                    promise.reject(E_LAYOUT_ERROR, Log.getStackTraceString(e));
+                } catch (Exception e) {
+                    Log.d("ReactNative", e.getMessage());
+                    promise.reject(E_LAYOUT_ERROR, Log.getStackTraceString(e));
+                }
+            }
+        });
     }
 
     @ReactMethod
     public void writeByte(ReadableMap map, Promise promise) {
-        operations.add("writeByte");
-        parameters.add(map);
-        promises.add(promise);
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    connect();
+                } catch (IOException e) {
+                    Log.d("ReactNative", e.getMessage());
+                    promise.reject(E_LAYOUT_ERROR, Log.getStackTraceString(e));
+                    return;
+                }
+                try {
+                    int sectorIndex = param.getInt("sector");
+                    int blockIndex = param.getInt("block");
+                    int byteIndex = param.getInt("byte");
+                    String data = param.getString("data");
+
+                    blockIndex = getRealBlockIndex(sectorIndex, blockIndex);
+                    Log.d("ReactNative", String.valueOf(sectorIndex) + "," + String.valueOf(blockIndex));
+                    auth(sectorIndex);
+
+                    byte[] blockBytes = tag.readBlock(blockIndex);
+                    if (byteIndex > blockBytes.length - 1) {
+                        throw new Exception("Out of bound: invalid byte index to wrtie");
+                    } else {
+                        blockBytes = overwriteBytesWithString(blockBytes, byteIndex, data);
+                        tag.writeBlock(blockIndex, blockBytes);
+                    }
+
+                    WritableMap map = Arguments.createMap();
+                    map.putString("payload", byteArrayToHexString(blockBytes));
+                    promise.resolve(map);
+
+                } catch (IOException e) {
+                    Log.d("ReactNative", e.getMessage());
+                    promise.reject(E_LAYOUT_ERROR, Log.getStackTraceString(e));
+                } catch (Exception e) {
+                    Log.d("ReactNative", e.getMessage());
+                    promise.reject(E_LAYOUT_ERROR, Log.getStackTraceString(e));
+                }
+            }
+        });
+    }
+
+    @ReactMethod
+    public void close(ReadableMap map, Promise promise) {
+        try {
+            if (tag == null) {
+                throw new IOException("沒有抓到標籤");
+            }
+            if (tag.isConnected()) {
+                tag.close();
+            }
+            WritableMap map = Arguments.createMap();
+            map.putBoolean("payload", true);
+            promise.resolve();
+        } catch (IOException e) {
+            Log.d("ReactNative", e.getMessage());
+            promise.reject(E_LAYOUT_ERROR, Log.getStackTraceString(e));
+        }
+    }
+
+    private void connect() throws IOException {
+        if (tag == null) {
+            throw new IOException("沒有抓到標籤");
+        }
+        if (tag.isConnected() == false) {
+            tag.connect();
+        }
+    }
+
+    private void auth(int sectorIndex) throws Exception {
+        if (authStatuses.get(sectorIndex) == true) {
+            return;
+        }
+        boolean passed = false;
+        byte[][] arrayKeys = new byte[5][6];
+        arrayKeys[0] = hexStringToByteArray(authKey);
+        arrayKeys[1] = arrayKeys[0];
+        arrayKeys[2] = MifareClassic.KEY_DEFAULT;
+        arrayKeys[3] = MifareClassic.KEY_MIFARE_APPLICATION_DIRECTORY;
+        arrayKeys[4] = MifareClassic.KEY_NFC_FORUM;
+
+        String[] arrayTypes = new String[5];
+        arrayTypes[0] = authType;
+        arrayTypes[1] = "A".equals(authType) ? "B" : "A";
+        arrayTypes[2] = "A";
+        arrayTypes[3] = "A";
+        arrayTypes[4] = "A";
+
+        for (int i = 0; i < arrayKeys.length; i++) {
+            try {
+                if ("A".equals(arrayTypes[i])) {
+                    passed = tag.authenticateSectorWithKeyA(sectorIndex, arrayKeys[i]);
+                } else {
+                    passed = tag.authenticateSectorWithKeyB(sectorIndex, arrayKeys[i]);
+                }
+            } catch (TagLostException e) {
+                throw e;
+            } catch (IOException e) {
+                Log.d("ReactNative", e.getMessage());
+            }
+            
+            if (passed) {
+                Log.d("ReactNative", "Sector " + String.valueOf(sectorIndex) + ", " + byteArrayToHexString(arrayKeys[i]));
+                break;
+            }
+        }
+        authStatuses.set(sectorIndex, passed);
+
+        if (passed == false) {
+            throw new IOException("Authentication failed: sector" + String.valueOf(sectorIndex) + ", type" + authType + ", key=" + authKey);
+        }
+    }
+
+    /**
+     * When you want to access block 7 in sector 2
+     * (which usually means the last block in sector)
+     * You'll probably get blockIndex as 7 or 3.
+     * 
+     * When you get blockIndex as 3, 
+     * you have to calculate the real blockIndex which Android API needs.
+     * 
+     * This function is designed to fix this.
+     */
+    private int getRealBlockIndex(int sectorIndex, int blockIndex) throws Exception {
+        int firstBlockInSector = tag.sectorToBlock(sectorIndex);
+        int blockCounts = tag.getBlockCountInSector(sectorIndex);
+        int lastBlockInSector = firstBlockInSector + blockCounts - 1;
+
+        if (firstBlockInSector <= blockIndex && blockIndex <= lastBlockInSector) {
+            // blockCounts = 4, sector = 3, block = 9
+            return blockIndex;
+        } else if (0 <= blockIndex && blockIndex <= blockCounts - 1) {
+            // blockCounts = 4, sector = 3, block = 0
+            return firstBlockInSector + blockIndex - 1;
+        } else {
+            throw new Exception(
+                "Out of bound: sector " + String.valueOf(sectorIndex) + ", block " + String.valueOf(blockIndex)
+            );
+        }
+    }
+
+    private byte[] overwriteBytesWithString(byte[] blockBytes, int byteIndex, String data)
+            throws UnsupportedEncodingException {
+        byte[] bytes = data.getBytes("UTF-8");
+        for (byte b : bytes) {
+            blockBytes[byteIndex] = b;
+            byteIndex++;
+        }
+        return blockBytes;
     }
 
     private void resetTagInfos(int sectorCount) {
         Log.d("ReactNative", "resetTagInfos");
         authStatuses = new ArrayList<Boolean>(Collections.nCopies(sectorCount, false));
-    }
-
-    private void clearQueue() {
-        Log.d("ReactNative", "clearQueue");
-        operations.clear();
-        parameters.clear();
-
-        while (promises.size() > 0) {
-            Promise promise = promises.remove();
-            promise.reject(E_LAYOUT_ERROR, "Clear queue.");
-        }
-    }
-
-    private void clearQueue(String message) {
-        Log.d("ReactNative", "clearQueue");
-        operations.clear();
-        parameters.clear();
-
-        while (promises.size() > 0) {
-            Promise promise = promises.remove();
-            promise.reject(E_LAYOUT_ERROR, message);
-        }
     }
 
     private static byte[] hexStringToByteArray(String s) {
